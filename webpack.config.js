3b4b83523838b8b4bf218c8c7d14cc435b8858b4
Darren <a@a.com>
2021-04-20 15:52:34 -0400

    prep for react ui

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const htmlplugin = new HtmlWebpackPlugin({
+  template: path.join(__dirname, 'src', 'web', 'index.html')
+})
+
+
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'web', 'index.js'),
+  mode: 'development',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'index.bundle.js'
+  },
+  resolve: {
+    modules: [path.resolve(__dirname, 'src'), 'node_modules']
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'src', 'web'),
+    compress: true,
+    port: 9000,
+  },
+  plugins: [
+    htmlplugin
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+}
