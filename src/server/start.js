f33ea849b9af6f78209339173dc8fa5274f3f78a
Darren <a@a.com>
2021-04-19 21:25:39 -0400

    update to move routes

--- src/server/start.js
+++ src/server/start.js
@@ -1,8 +1,9 @@
 const express = require('express')
+const routes = require('./api/routes')
 
-
+const port = process.env.HTTP_SERVER_PORT || 8080
 const app = express()
-
+app.use(routes)
 app.use(express.static('public'))
 
 // simple logging route
@@ -13,12 +14,5 @@ app.use((req, res, next) => {
   next()
 })
 
-
-
-
-
-
-
-const port = process.env.HTTP_SERVER_PORT || 8080
 
 app.listen(port, () => console.log(`Started on ${port}`))
