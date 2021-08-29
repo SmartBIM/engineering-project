3328d7c1eaf7778b7f13f0367be0f2185fb6e5ee
Sean <sgilmore@michael-sean.com>
2021-08-29 14:08:49 -0400

    Added react-redux redux redux-thunk and ../server/database/SiteConfigs.js

---
+++ server/database/SiteConfigs.js
@@ -0,0 +1,19 @@
+const siteconfigs = [
+  {
+    id: 1,
+    store: 'Red Store',
+    primaryColor: 'red'
+  },
+  {
+    id: 2,
+    store: 'White Store',
+    primaryColor: 'white'
+  },
+  {
+    id: 3,
+    store: 'Blue Store',
+    primaryColor: 'blue'
+  }
+];
+
+module.exports = siteconfigs;
